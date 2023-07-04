fbcb148411927295ffff1a554dea04e15c262022
DannielNavas <dannielnavas@gmail.com>
2023-07-04 08:14:52 -0500

    ✨ uso externo de pinia

--- src/core/auth.js
+++ src/core/auth.js
@@ -1,19 +1,21 @@
-import useProfileStore from '@/stores/profile.js'
+import useProfileStore from "@/stores/profile.js";
 
+// TODO: llamada a la api de login
 const login = async () => {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve({
-        username: 'nerd diana'
-      })
-    }, 2500)
-  })
-}
+        username: "nerd diana",
+      });
+    }, 2500);
+  });
+};
 
+// TODO: llamada al backend para obtener los datos del usuario logueado
 export const main = async () => {
-  const profileStore = useProfileStore()
-  const user = await login()
-  console.log(user)
-  profileStore.username = user.username
-  profileStore.status = 'active'
-}
+  const profileStore = useProfileStore();
+  const user = await login();
+  console.log(user);
+  profileStore.username = user.username;
+  profileStore.status = "active";
+};
--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import App from "./App.vue";
 import "./index.css";
 import router from "./router";
 
+// import useProfileStore from "./stores/profile.js";
+
 const app = createApp(App);
 
 const pinia = createPinia();
@@ -14,6 +16,10 @@ app.use(router);
 app.use(pinia);
 app.component("Icon", Icon);
 
+// const profileStore = useProfileStore();
+
+// console.log(profileStore);
+// TODO: se llama a la funcion main para obtener los datos del usuario logueado POR BUENA PARACTICA SE HACE EN EL MAIN POR UN ARCHIO POR FUERA DE MAIN DE VUE
 main();
 
 app.mount("#app");
